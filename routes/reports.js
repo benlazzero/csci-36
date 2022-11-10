1ea70a33d816346ccdb04daaea234bc58369c8a9
Ubuntu <ubuntu@ip-172-31-11-189.ec2.internal>
2022-11-10 19:01:07 +0000

    clearing

--- routes/reports.js
+++ routes/reports.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { poutcomes } from '../database/sequelize.js';
 import { assessments } from '../database/sequelize.js';
 
 const router = express.Router();
@@ -8,5 +9,27 @@ router.get("/reports", async (req, res) => {
     res.render("reports", { title: "SLO Tracker - Reports",
      assessments:allAssessments });
 });
+//single_report
+router.get("/reports/:discussion_id", async (req, res) => {
+    const theAssessment = await assessments.findOne({
+        where: {
+            prog_id: req.params.program_id
+        },
+        raw: true
+    });
+    const allOutcomes =  await poutcomes.findAll({order: ['pout_id'], raw: true});
+    if (theAssessment == null) {
+        res.status(404).json('ERROR: No program found with that id')
+    } else {
+        console.log("Found: " + theAssessment.discussion_id)
+        res.render("single_report", { 
+            assessment: theAssessment, 
+            title: `Report - ${theAssessment.assessed_program}`,
+            poutcomes: allOutcomes
+        });
+    }
+});
+
+
 
 export default router;
