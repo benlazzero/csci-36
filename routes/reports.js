666f43ca7020a687cebeb6c2c4bb75a17043685d
Ubuntu <ubuntu@ip-172-31-11-189.ec2.internal>
2022-11-12 19:33:01 +0000

    styling single_report.ejs

--- routes/reports.js
+++ routes/reports.js
@@ -1,31 +1,30 @@
 import express from 'express';
-import { poutcomes } from '../database/sequelize.js';
 import { assessments } from '../database/sequelize.js';
 
 const router = express.Router();
 
 router.get("/reports", async (req, res) => {
   const allAssessments =  await assessments.findAll({order: ['discussion_id'],raw: true});
-    res.render("reports", { title: "SLO Tracker - Reports",
+    res.render("reports", { title: "Submitted Reports",
      assessments:allAssessments });
 });
+
+
 //single_report
 router.get("/reports/:discussion_id", async (req, res) => {
     const theAssessment = await assessments.findOne({
         where: {
-            disscussion_id: req.params.discussion_id
+            discussion_id: req.params.discussion_id
         },
         raw: true
     });
-    const allOutcomes =  await poutcomes.findAll({order: ['pout_id'], raw: true});
     if (theAssessment == null) {
-        res.status(404).json('ERROR: No program found with that id')
+        res.status(404).json('ERROR: No report/assessment found with that id')
     } else {
         console.log("Found: " + theAssessment.discussion_id)
-        res.render("single_report", { 
+        res.render("single_reports", { 
             assessment: theAssessment, 
-            title: `Report - ${theAssessment.assessed_program}`,
-            poutcomes: allOutcomes
+            title: `Report - ${theAssessment.assessed_program}`
         });
     }
 });
